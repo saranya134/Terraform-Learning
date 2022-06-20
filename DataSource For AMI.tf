b5560e717fb2fff94c1532305b67bd2408806121
saranya134 <39684653+saranya134@users.noreply.github.com>
2022-06-20 17:40:24 +0530

    Create DataSource For AMI.tf

---
+++ DataSource For AMI.tf
@@ -0,0 +1,24 @@
+// I created this data block for getting the latest AMI for EC2
+data "aws_ami" "AMIFOREC2" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
