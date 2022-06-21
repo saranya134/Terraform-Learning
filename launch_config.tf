26297a8b1e4443762170f9daa07431a167e072d2
saranya134 <39684653+saranya134@users.noreply.github.com>
2022-06-21 09:25:52 +0530

    Update launch_config.tf

--- launch_config.tf
+++ launch_config.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_configuration" "launchconfig" {
   name            = "instance-launch"
-  image_id        = "ami-09439f09c55136ecf"
+  image_id        = "ami-065deacbcaac64cf2"
   instance_type   = "t2.small"
   name_prefix = "testing"
   security_groups = [aws_security_group.securitygroup.id]
